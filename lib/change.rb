214fe31536a75a2a605a3c4d124f0384ea74fbff
Matthew Giem and Molly McGlone <matthew+molly@>
2016-06-30 10:44:18 -0700

    completes change project

--- lib/change.rb
+++ lib/change.rb
@@ -7,15 +7,32 @@ class Fixnum
     answer = ""
     if (entry./(25)).>(0)
       quarters = entry/25
-      num_quarters = quarters.to_s+(" quarters")
+      num_quarters = quarters.to_s+(" quarter") if quarters == 1
+      num_quarters = quarters.to_s+(" quarters") if quarters > 1
       answer.concat(num_quarters)
+      answer.concat(" ") if entry.%(25) > 0
       entry = entry.%(25)
     end
     if (entry./(10)).>(0)
       dimes = entry/10
-      num_dimes = dimes.to_s+(" dimes")
+      num_dimes = dimes.to_s+(" dime") if dimes == 1
+      num_dimes = dimes.to_s+(" dimes") if dimes > 1
       answer.concat(num_dimes)
+      answer.concat(" ") if entry.%(10) > 0
       entry = entry.%(10)
+    end
+    if (entry./(5)).>(0)
+      nickles = entry/5
+      num_nickles = nickles.to_s+(" nickle")
+      answer.concat(num_nickles)
+      answer.concat(" ") if entry.%(5) > 0
+      entry = entry.%(5)
+    end
+    if (entry./(1)).>(0)
+      pennies = entry/1
+      num_pennies = pennies.to_s+(" pennies") if pennies > 1
+      num_pennies = pennies.to_s+(" penny") if pennies == 1
+      answer.concat(num_pennies)
     end
     answer
   end
--- spec/change_integration_spec.rb
+++ spec/change_integration_spec.rb
@@ -1,14 +1,13 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('the winner path', {:type => :feature}) do
-#   it('processes what player one and player two have and decides who wins') do
-#     visit('/')
-#     select('rock', :from => 'player_one')
-#     select('paper', :from => 'player_two')
-#     click_button('send')
-#     expect(page).to have_content('loses')
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the change path', {:type => :feature}) do
+  it('accepts a number from the user and converts it into the fewest coins possible to represent that amount of change') do
+    visit('/')
+    fill_in('cents', :with => 92)
+    click_button('Go')
+    expect(page).to have_content('3 quarters 1 dime 1 nickle 2 pennies')
+  end
+end
