2cf15db5c4c2f33c2accdc8003bfce10531f8b2c
Matthew Giem and Molly McGlone <matthew@>
2016-06-30 10:09:36 -0700

    add code to pass first two specs to handle quarters and dimes

--- lib/change.rb
+++ lib/change.rb
@@ -1,8 +1,22 @@
 
 require('pry')
 
-class String
+class Fixnum
   define_method(:change)  do
-    
+    entry = self
+    answer = ""
+    if (entry./(25)).>(0)
+      quarters = entry/25
+      num_quarters = quarters.to_s+(" quarters")
+      answer.concat(num_quarters)
+      entry = entry.%(25)
+    end
+    if (entry./(10)).>(0)
+      dimes = entry/10
+      num_dimes = dimes.to_s+(" dimes")
+      answer.concat(num_dimes)
+      entry = entry.%(10)
+    end
+    answer
   end
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -2,8 +2,20 @@
 require("rspec")
 require("change.rb")
 
-describe('String#chagne') do
-  it('') do
-    expect(''.change("")).to(eq(""))
+describe('Fixnum#change') do
+  it('take a change value less than 5 and give how many pennies you need') do
+    expect(3.change()).to(eq("3 pennies"))
+  end
+  it('take a change value thats a mulitple of 5 and give how many nickles you need') do
+    expect(5.change()).to(eq("1 nickle"))
+  end
+  it('take a change value thats a mulitple of 10 and give how many dimes you need') do
+    expect(20.change()).to(eq("2 dimes"))
+  end
+  it('take a change value thats a mulitple of 25 and give how many quarters you need') do
+    expect(75.change()).to(eq("3 quarters"))
+  end
+  it('take a change value thats less than 100 and give you the least amout of coins you need') do
+    expect(88.change()).to(eq("3 quarters 1 dime 3 pennies"))
   end
 end
